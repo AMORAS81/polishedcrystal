dc91193c8aaeee3821603e46b6eed3e1a7ae347f
AMORAS81 <markamoras81@gmail.com>
2025-02-18 10:43:07 -0300

    abilities.asm

--- engine/battle/abilities.asm
+++ engine/battle/abilities.asm
@@ -1657,6 +1657,7 @@ DefensiveDamageAbilities:
 	dbw THICK_FAT, EnemyThickFatAbility
 	dbw DRY_SKIN, EnemyDrySkinAbility
 	dbw FUR_COAT, EnemyFurCoatAbility
+	dbw FLUFFY, EnemyFluffyAbility
 	dbw -1, -1
 
 TechnicianAbility:
@@ -1927,6 +1928,22 @@ EnemyFurCoatAbility:
 ; Doubles physical Defense
 	ln a, 1, 2 ; 1/2 = 50%
 	jmp ApplyPhysicalDefenseDamageMod
+
+EnemyFluffyAbility:
+    ld a, BATTLE_VARS_MOVE_TYPE
+    call GetBattleVar
+    cp FIRE
+    jr nz, .notFire
+    call CheckContactMove
+    ret nc
+    ln a, 2, 1 ; x2
+    jmp MultiplyAndDivide
+
+.notFire
+    call CheckContactMove
+    ret c
+    ln a, 1, 2 ; x0.5
+    jmp MultiplyAndDivide
 
 HydrationAbility:
 	call GetWeatherAfterUserUmbrella
